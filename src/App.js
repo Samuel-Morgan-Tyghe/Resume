ab0f45ea55750c4be48a4c365ddad995305dd074
Samuel Morgan-Tyghe <sa>
2020-11-23 18:38:55 +0000

    undraggable, remove border

--- src/App.js
+++ src/App.js
@@ -245,106 +245,127 @@ class App extends Component {
             options={{ max: 55, easing: "cubic-bezier(.03,.98,.52,.99)" }}
           >
             <img
+              draggable="false"
               className="Tilt-inner TZ1"
               src={this.state.imageVarSize[0]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ2"
               src={this.state.imageVarSize[1]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ3"
               src={this.state.imageVarSize[2]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ4"
               src={this.state.imageVarSize[3]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ5"
               src={this.state.imageVarSize[4]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ6"
               src={this.state.imageVarSize[5]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ7"
               src={this.state.imageVarSize[6]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ8"
               src={this.state.imageVarSize[7]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ9"
               src={this.state.imageVarSize[8]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ10"
               src={this.state.imageVarSize[9]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ11"
               src={this.state.imageVarSize[10]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ12"
               src={this.state.imageVarSize[11]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ13"
               src={this.state.imageVarSize[12]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ14"
               src={this.state.imageVarSize[13]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ15"
               src={this.state.imageVarSize[14]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ16"
               src={this.state.imageVarSize[15]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ17"
               src={this.state.imageVarSize[16]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ18"
               src={this.state.imageVarSize[17]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ19"
               src={this.state.imageVarSize[18]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ20"
               src={this.state.imageVarSize[19]}
               alt="collage-art"
             />
             <img
+              draggable="false"
               className="Tilt-inner TZ21"
               src={this.state.imageVarSize[20]}
               alt="collage-art"
